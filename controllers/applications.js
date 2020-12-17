1c90d9b144fb005ccf584c36a6944496f160a5e4
claywatkins8 <claywatkins8@gmail.com>
2020-12-17 14:30:27 -0800

    applications ID page working

--- controllers/applications.js
+++ controllers/applications.js
@@ -25,7 +25,7 @@ router.get("/", function(req,res){
 
 // Application Show
 router.get("/:id", function(req,res){
-    const id = request.params.id;
+
 
     // db.Application.findById(id, function(err,foundApplication){
     //     if(err)  return res.send(err);
@@ -38,29 +38,28 @@ router.get("/:id", function(req,res){
     //  });
 
     db.Application
-    .findById(id)
+    .findById(req.params.id)
     .populate("companies")
     .exec(function (err, foundApplication) {
       if (err) return res.send(err);
       
       const context = { application: foundApplication };
       return res.render("applications/applicationShowPage", context);
-    })
+    });
   
 });
 
 
 // Application Edit
-router.get("/:id/edit", function(req,res){
-    const id = request.params.id;
-
-    db.Application.findById(id, function (err, foundApplication) {
+router.get("/:id/edit", function (req, res) {
+	db.Application.findById(req.params.id, function (err, foundApplication) {
 		if (err) return res.send(err);
 
 		const context = { application: foundApplication };
 		res.render("applications/applicationEditPage", context);
 	});
 });
+
 
 // Update
 router.put("/:id", function(req,res){
