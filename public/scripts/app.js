0431025640265ba088b43a8b9ededa114f66123c
claywatkins8 <claywatkins8@gmail.com>
2020-12-22 16:00:29 -0800

    end of day commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,16 @@
 console.log("Welcome to the medium killer.");
 
 
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+};
